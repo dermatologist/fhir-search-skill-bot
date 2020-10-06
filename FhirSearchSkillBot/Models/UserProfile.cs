5a2bb3f27893a3c230e8eed994dd564faeb6f465
Bellraj Eapen <github@gulfdoctor.net>
2020-10-06 17:11:18 +0000

    Before adding to startup

---
+++ FhirSearchSkillBot/Models/UserProfile.cs
@@ -0,0 +1,10 @@
+public class UserProfile
+{
+    public string Transport { get; set; }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public Attachment Picture { get; set; }
+}
