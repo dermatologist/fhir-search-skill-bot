8c36b9efa6c5add0bcababcef632fc9e6d41e9b2
beapen <github@gulfdoctor.net>
2020-10-07 18:37:58 +0000

    Before http call

--- FhirSearchSkillBot.testing/FhirSearchModelTest.cs
+++ FhirSearchSkillBot.testing/FhirSearchModelTest.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
-using 
+using FhirSearchSkillBot.Models; 
 namespace FhirSearchSkillBot.testing
 {
     public class FhirSearchModelTest
     {
         [Fact]
-        public void Test1()
+        public void TestModel()
         {
-
+            var _fhirSearchModel = new FhirSearchModel();
+            _fhirSearchModel.Patient = "23";
+            _fhirSearchModel.SearchResource = "Encounter";
+            _fhirSearchModel.SearchParam.Add("_date");
+            _fhirSearchModel.SearchQualifier.Add("=");
+            _fhirSearchModel.SearchValue.Add("12-03-2020");
+            Console.Write(_fhirSearchModel.FhirSearchString);
         }
     }
 }
--- FhirSearchSkillBot/Models/FhirSearchModel.cs
+++ FhirSearchSkillBot/Models/FhirSearchModel.cs
@@ -11,42 +11,61 @@ namespace FhirSearchSkillBot.Models
     /// </summary>
     public class FhirSearchModel
     {
-    public string BaseUrl { get; set; }
+    
+        public string BaseUrl { get; set; }
 
-    /// <summary>
-    /// Patient ID if the search has _subject or Resource is Patient
-    /// </summary>
-    public string Patient { get; set; }
+        /// <summary>
+        /// Patient ID if the search has _subject or Resource is Patient
+        /// </summary>
+        public string Patient { get; set; }
 
-    /// <summary>
-    /// The FHIR resource that is being searched
-    /// </summary>
-    public string SearchResource { get; set; }
+        /// <summary>
+        /// The FHIR resource that is being searched
+        /// </summary>
+        public string SearchResource { get; set; }
 
-    public List<string> SearchParam { get; set; }
+        public List<string> SearchParam { get; set; }
 
-    public List<string> SearchQualifier { get; set; }
+        public List<string> SearchQualifier { get; set; }
 
-    public List<string> SearchValue { get; set; }
+        public List<string> SearchValue { get; set; }
+        
+        
+        private string _fhirSearchString = "";
+        //Constructor
+        public FhirSearchModel()
+        {
+            SearchParam = new List<string>();
+            SearchQualifier = new List<string>();
+            SearchValue = new List<string>();
 
-    public string FhirSearchString {
-        get{
-            var _fhirSearchString = "?";
-            if(Patient != null){
-                _fhirSearchString = "_subject=" + Patient;
-                if (SearchResource == null)
-                {
-                    _fhirSearchString = "Patient/_subject=" + Patient;
-                }
-            }
-            _fhirSearchString += SearchResource + "/";
-            // Iteratively add all search parameters
-            for (int i = 0; i < SearchParam.Count; i++){
-                _fhirSearchString += SearchParam[i] + SearchQualifier[i] + SearchValue[i] + "&";
-            }
-            return BaseUrl + _fhirSearchString;
         }
-    }
+
+        /// <summary>
+        /// Renders the FHIR search string
+        /// </summary>
+        public string FhirSearchString {
+            get{
+                
+                    if (SearchResource == null)
+                    {
+                        _fhirSearchString += "Patient?_id=" + Patient + "&";
+                    }
+                
+                    _fhirSearchString += SearchResource + "?";
+                    // Iteratively add all search parameters
+                    for (int i = 0; i < SearchParam.Count; i++){
+                        _fhirSearchString += SearchParam[i] + SearchQualifier[i] + SearchValue[i] + "&";
+                    }
+                    if (Patient != null)
+                    {
+                        _fhirSearchString += "_subject=" + Patient;
+                    }
+                return BaseUrl + _fhirSearchString;
+            }
+        }
+
+        
 
     }
 }
